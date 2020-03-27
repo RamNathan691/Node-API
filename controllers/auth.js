d2aa77f29a17511200e2559aff857bc4e403028f
RamNathan691 <ramnathan0691@gmail.com>
2020-03-27 13:10:30 +0530

    Generated a token using a middleware in THE USERS MODEL called getSignedJwtToken()method not a static method

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,7 +6,7 @@ const AsyncHandler = require('../middleware/async')
 // @route GET/api/v1/auth/register
 // @access Public
 exports.register = AsyncHandler(async (req, res, next) => {
-  const { name, email, password , role } = req.body
+  const { name, email, password, role } = req.body
 
   // create user
   const user = await User.create({
@@ -15,5 +15,7 @@ exports.register = AsyncHandler(async (req, res, next) => {
     password,
     role
   })
-  res.status(200).json({ success:true })
+  // CREATE TOKEN
+  const token = user.getSignedJwtToken()
+  res.status(200).json({ success: true, token: token })
 })
